ac2f99220a4e6718a01e9b7d050f103fdc116f7a
psandana <psandana@gmail.com>
2024-01-03 02:21:54 +0000

    adding etw exporter

---
+++ opentelemetry-etw-logs/src/logs/converters.rs
@@ -0,0 +1,37 @@
+use opentelemetry::logs::AnyValue;
+use opentelemetry::{Key, OrderMap};
+use serde_json::{json, Map, Value};
+
+pub(super) trait IntoJson {
+    fn as_json_value(&self) -> Value;
+}
+
+impl IntoJson for AnyValue {
+    fn as_json_value(&self) -> Value {
+        match &self {
+            AnyValue::Int(value) => json!(value),
+            AnyValue::Double(value) => json!(value),
+            AnyValue::String(value) => json!(value.to_string()),
+            AnyValue::Boolean(value) => json!(value),
+            AnyValue::Bytes(_value) => todo!("No support for AnyValue::Bytes yet."),
+            AnyValue::ListAny(value) => value.as_json_value(),
+            AnyValue::Map(value) => value.as_json_value(),
+        }
+    }
+}
+
+impl IntoJson for OrderMap<Key, AnyValue> {
+    fn as_json_value(&self) -> Value {
+        Value::Object(
+            self.iter()
+                .map(|(k, v)| (k.to_string(), v.as_json_value()))
+                .collect::<Map<String, Value>>(),
+        )
+    }
+}
+
+impl IntoJson for [AnyValue] {
+    fn as_json_value(&self) -> Value {
+        Value::Array(self.iter().map(IntoJson::as_json_value).collect())
+    }
+}
