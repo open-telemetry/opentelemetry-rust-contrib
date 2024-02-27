cec13bc568ff1ea7cc40a337fb8bc7257753d7b3
Matthew Boddewyn <31598686+mattbodd@users.noreply.github.com>
2024-02-27 13:59:28 -0800

    Add simple tests to etw module

--- opentelemetry-etw-metrics/src/etw/mod.rs
+++ opentelemetry-etw-metrics/src/etw/mod.rs
@@ -17,6 +17,7 @@ lazy_static! {
     static ref REGISTERED_PROVIDERS: Mutex<HashSet<String>> = Mutex::new(HashSet::new());
 }
 
+#[derive(Debug)]
 pub struct ProviderGuard {
     provider: Pin<Box<tld::Provider>>,
 }
@@ -61,12 +62,12 @@ impl ProviderGuard {
         // happen after `register`ing is complete.
         match unsafe { provider.as_ref().register() } {
             0 => {
-                println!("Successfully registered ETW provider")
+                println!("Successfully registered ETW provider named: {}", name);
             }
             error_code => {
                 global::handle_error(MetricsError::Other(format!(
-                    "Failed to register ETW provider with error code: {}",
-                    error_code
+                    "Failed to register ETW provider named {} with error code: {}",
+                    name, error_code
                 )));
             }
         }
@@ -95,10 +96,12 @@ impl ProviderGuard {
     }
 
     /// Unregister the provider.
-    /// TODO: Figure out how to make sure that we unregister even if the process is killed or the library is unloaded.
     pub fn unregister(&self) {
         match self.provider.unregister() {
-            0 => println!("Successfully unregistered ETW provider"),
+            0 => println!(
+                "Successfully unregistered ETW provider named {}",
+                self.provider.name()
+            ),
             error_code => eprintln!(
                 "Failed to unregister ETW provider with error code: {}",
                 error_code
@@ -106,7 +109,12 @@ impl ProviderGuard {
         }
 
         let mut registered_providers = REGISTERED_PROVIDERS.lock().unwrap();
-        registered_providers.remove(self.provider.name());
+        if !registered_providers.remove(self.provider.name()) {
+            eprintln!(
+                "Could not remove provider name from set of registered provider names as the set did not contain provider name: {}",
+                self.provider.name()
+            );
+        }
     }
 }
 
@@ -115,5 +123,51 @@ impl Drop for ProviderGuard {
         // `Provider::Unregister` is called when a `Provider` is dropped, so we must ensure to remove the provider name
         // from the set of registered providers.
         self.unregister();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{ProviderGuard, REGISTERED_PROVIDERS};
+
+    use opentelemetry::metrics::MetricsError;
+
+    #[test]
+    fn register() {
+        let _provider = ProviderGuard::register("provider_name", None, None).unwrap();
+
+        {
+            let registered_providers = REGISTERED_PROVIDERS.lock().unwrap();
+            assert!(registered_providers.contains("provider_name"));
+        }
+    }
+
+    #[test]
+    fn duplicate_provider_name_registration_fails() {
+        let _provider = ProviderGuard::register("duplicate_provider_name", None, None).unwrap();
+
+        let result = ProviderGuard::register("duplicate_provider_name", None, None);
+
+        assert!(result.is_err());
+        assert_eq!(result
+            .unwrap_err().to_string(), MetricsError::Other("Failed to register ETW provider named duplicate_provider_name: provider with the same name already registered".to_string()).to_string());
+    }
+
+    #[test]
+    fn multiple_unregister_calls_succeed() {
+        let provider = ProviderGuard::register("multiple_unregister_calls", None, None).unwrap();
+
+        {
+            let registered_providers = REGISTERED_PROVIDERS.lock().unwrap();
+            assert!(registered_providers.contains("multiple_unregister_calls"));
+        }
+
+        provider.unregister();
+        provider.unregister();
+
+        {
+            let registered_providers = REGISTERED_PROVIDERS.lock().unwrap();
+            assert!(!registered_providers.contains("multiple_unregister_calls"));
+        }
     }
 }
