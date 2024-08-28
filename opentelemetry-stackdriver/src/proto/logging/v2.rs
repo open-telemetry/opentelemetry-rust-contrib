d7d9af5719383f36473479aa370afef7b090dafe
Gary Coady <gary@lyranthe.org>
2024-08-28 19:41:08 +0200

    Refresh generated code in opentelemetry-stackdriver (#102)

--- opentelemetry-stackdriver/src/proto/logging/v2.rs
+++ opentelemetry-stackdriver/src/proto/logging/v2.rs
@@ -654,8 +654,8 @@ pub mod logging_service_v2_client {
     where
         T: tonic::client::GrpcService<tonic::body::BoxBody>,
         T::Error: Into<StdError>,
-        T::ResponseBody: Body<Data = Bytes> + Send + 'static,
-        <T::ResponseBody as Body>::Error: Into<StdError> + Send,
+        T::ResponseBody: Body<Data = Bytes> + std::marker::Send + 'static,
+        <T::ResponseBody as Body>::Error: Into<StdError> + std::marker::Send,
     {
         pub fn new(inner: T) -> Self {
             let inner = tonic::client::Grpc::new(inner);
@@ -679,7 +679,7 @@ pub mod logging_service_v2_client {
                 >,
             >,
             <T as tonic::codegen::Service<http::Request<tonic::body::BoxBody>>>::Error:
-                Into<StdError> + Send + Sync,
+                Into<StdError> + std::marker::Send + std::marker::Sync,
         {
             LoggingServiceV2Client::new(InterceptedService::new(inner, interceptor))
         }
