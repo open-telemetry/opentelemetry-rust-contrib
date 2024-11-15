d431f2eb40392956aeadf1f842f400838766f427
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2024-11-15 06:34:27 +0000

    update to 0.27

--- opentelemetry-user-events-metrics/examples/basic-metrics.rs
+++ opentelemetry-user-events-metrics/examples/basic-metrics.rs
@@ -32,27 +32,31 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .f64_counter("counter_f64_test")
         .with_description("test_decription")
         .with_unit("test_unit")
-        .init();
+        .build();
 
     let counter2 = meter
         .u64_counter("counter_u64_test")
         .with_description("test_decription")
         .with_unit("test_unit")
-        .init();
+        .build();
 
     // Create an UpDownCounter Instrument.
-    let updown_counter = meter.i64_up_down_counter("up_down_counter_i64_test").init();
-    let updown_counter2 = meter.f64_up_down_counter("up_down_counter_f64_test").init();
+    let updown_counter = meter
+        .i64_up_down_counter("up_down_counter_i64_test")
+        .build();
+    let updown_counter2 = meter
+        .f64_up_down_counter("up_down_counter_f64_test")
+        .build();
 
     // Create a Histogram Instrument.
     let histogram = meter
         .f64_histogram("histogram_f64_test")
         .with_description("test_description")
-        .init();
+        .build();
     let histogram2 = meter
         .u64_histogram("histogram_u64_test")
         .with_description("test_description")
-        .init();
+        .build();
 
     // Create a ObservableGauge instrument and register a callback that reports the measurement.
     let _gauge = meter
@@ -68,7 +72,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 ],
             )
         })
-        .init();
+        .build();
 
     let _gauge2 = meter
         .u64_observable_gauge("observable_gauge_u64_test")
@@ -83,7 +87,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 ],
             )
         })
-        .init();
+        .build();
 
     // Create a ObservableCounter instrument and register a callback that reports the measurement.
     let _observable_counter = meter
@@ -99,7 +103,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 ],
             )
         })
-        .init();
+        .build();
 
     let _observable_counter2 = meter
         .f64_observable_counter("observable_counter_f64_test")
@@ -114,7 +118,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 ],
             )
         })
-        .init();
+        .build();
 
     // Create a Observable UpDownCounter instrument and register a callback that reports the measurement.
     let _observable_up_down_counter = meter
@@ -130,7 +134,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 ],
             )
         })
-        .init();
+        .build();
     let _observable_up_down_counter2 = meter
         .f64_observable_up_down_counter("observable_up_down_counter_f64_test")
         .with_description("test_description")
@@ -144,7 +148,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 ],
             )
         })
-        .init();
+        .build();
 
     loop {
         // Record measurements using the Counter instrument.
