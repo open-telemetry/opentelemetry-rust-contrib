64255ab6400e8819a9c182f2c9fee25f5cf663c0
Matthew Boddewyn <31598686+mattbodd@users.noreply.github.com>
2024-11-26 12:42:11 -0800

    Add doc comment to etw bench

--- opentelemetry-etw-metrics/benches/etw.rs
+++ opentelemetry-etw-metrics/benches/etw.rs
@@ -1,3 +1,18 @@
+//! run with `$ cargo bench --bench etw -- --exact <test_name>` to run specific test for logs
+//! So to run test named "fibonacci" you would run `$ cargo bench --bench etw -- --exact fibonacci`
+//! To run all tests for logs you would run `$ cargo bench --bench etw`
+//!
+/*
+The benchmark results:
+criterion = "0.5.1"
+OS:
+Hardware:
+RAM:
+| Test                           | Average time|
+|--------------------------------|-------------|
+|                                |             |
+*/
+
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn fibonacci(n: u64) -> u64 {
