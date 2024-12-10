f972a4c6f7c8cfc457fa7831945f990d285a64b9
Lalit <lalit_fin@yahoo.com>
2024-12-10 14:48:46 +0000

    fix benchmark

--- opentelemetry-user-events-logs/benches/benchmark.rs
+++ opentelemetry-user-events-logs/benches/benchmark.rs
@@ -1,3 +1,21 @@
+// Run as root user as access to `tracefs` is required, or ensure the current user has the necessary permissions.
+// To run benchmarks with root privileges, execute:
+//   sudo -E /home/<username>/.cargo/bin/cargo bench
+// Replace <username> with your actual username.
+//
+// System Information:
+// Processor: AMD EPYC 7763 64-Core Processor
+// CPU Cores: 8
+// Logical Processors: 16
+// Memory: 64 GB
+
+// provider_find_set_single time:   [3.9862 ns 3.9898 ns 3.9937 ns]
+// provider_find_set_concurrent_0threads time:   [3.9937 ns 3.9978 ns 4.0024 ns]
+// provider_find_set_concurrent_2threads:  time:    time:   [111.52 ns 114.61 ns 117.38 ns]
+// provider_find_set_concurrent_4threads: time:   time:   [199.18 ns 203.92 ns 208.43 ns]
+// provider_find_set_concurrent_8threads time:   time:   [199.18 ns 203.92 ns 208.43 ns]
+
+use criterion::black_box;
 use criterion::{criterion_group, criterion_main, Criterion};
 use eventheader_dynamic::{Provider, ProviderOptions};
 use std::sync::{
@@ -22,7 +40,7 @@ fn benchmark_find_set_single(c: &mut Criterion) {
     c.bench_function("provider_find_set_single", |b| {
         b.iter(|| {
             if let Some(event_set) = provider.find_set(level.into(), keyword) {
-                event_set.enabled(); // Check if the tracepoint is being listened to
+                black_box(event_set.enabled()); // Check if the tracepoint is being listened to
             }
         });
     });
@@ -30,7 +48,7 @@ fn benchmark_find_set_single(c: &mut Criterion) {
 
 /// Benchmark `find_set` with a parameterized number of concurrent threads
 fn benchmark_find_set_concurrent(c: &mut Criterion) {
-    let thread_counts = [2, 4, 8]; // Test with 2, 4, and 8 threads
+    let thread_counts = [0, 2, 4, 8]; // Test with 2, 4, and 8 threads
 
     for &thread_count in &thread_counts {
         // Setup the Provider
@@ -55,7 +73,7 @@ fn benchmark_find_set_concurrent(c: &mut Criterion) {
             worker_handles.push(thread::spawn(move || {
                 while !stop_flag_clone.load(Ordering::Relaxed) {
                     if let Some(event_set) = provider_clone.find_set(level.into(), keyword) {
-                        event_set.enabled(); // Check if tracepoint is being listened to
+                        black_box(event_set.enabled()); // Check if tracepoint is being listened to
                     }
                 }
             }));
@@ -69,7 +87,7 @@ fn benchmark_find_set_concurrent(c: &mut Criterion) {
         c.bench_function(&benchmark_name, |b| {
             b.iter(|| {
                 if let Some(event_set) = provider_ref.find_set(level.into(), keyword) {
-                    event_set.enabled(); // Check if tracepoint is being listened to
+                    black_box(event_set.enabled()); // Check if tracepoint is being listened to
                 }
             });
         });
