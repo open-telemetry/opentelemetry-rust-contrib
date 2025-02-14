8cff189e8d22db6a97fa630bb479b2374f5416c3
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2025-02-13 21:59:57 -0800

    enable logs in example

--- opentelemetry-user-events-logs/examples/basic-logs.rs
+++ opentelemetry-user-events-logs/examples/basic-logs.rs
@@ -5,7 +5,7 @@ use opentelemetry_sdk::logs::LoggerProvider;
 use opentelemetry_user_events_logs::{ExporterConfig, ReentrantLogProcessor, UserEventsExporter};
 use std::collections::HashMap;
 use tracing::error;
-use tracing_subscriber::prelude::*;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Layer};
 
 fn init_logger() -> LoggerProvider {
     let exporter_config = ExporterConfig {
@@ -21,6 +21,14 @@ fn init_logger() -> LoggerProvider {
 
 fn main() {
     // Example with tracing appender.
+    // Create a new tracing::Fmt layer to print the logs to stdout. It has a
+    // default filter of `info` level and above, and `debug` and above for logs
+    // from OpenTelemetry crates. The filter levels can be customized as needed.
+    let filter_fmt = EnvFilter::new("info").add_directive("opentelemetry=debug".parse().unwrap());
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .with_thread_names(true)
+        .with_filter(filter_fmt);
+    tracing_subscriber::registry().with(fmt_layer).init();
     let logger_provider = init_logger();
     let layer = layer::OpenTelemetryTracingBridge::new(&logger_provider);
     tracing_subscriber::registry().with(layer).init();
