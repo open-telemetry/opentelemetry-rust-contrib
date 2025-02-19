32cb39c4c021c0806913abaf795cb2c5f0e2ae3a
Cijo Thomas <cijo.thomas@gmail.com>
2025-02-18 18:18:09 -0800

    user export onfig

--- opentelemetry-user-events-logs/src/logs/mod.rs
+++ opentelemetry-user-events-logs/src/logs/mod.rs
@@ -1,4 +1,6 @@
-use exporter::UserEventsExporter;
+use std::collections::HashMap;
+
+use exporter::{ExporterConfig, UserEventsExporter};
 use opentelemetry_sdk::logs::LoggerProviderBuilder;
 use reentrant_logprocessor::ReentrantLogProcessor;
 
@@ -13,7 +15,11 @@ pub trait UserEventsLoggerProviderBuilderExt {
 }
 impl UserEventsLoggerProviderBuilderExt for LoggerProviderBuilder {
     fn with_user_event_exporter(self, provider_name: &str) -> Self {
-        let exporter = UserEventsExporter::new(provider_name, None, Default::default());
+        let exporter_config = ExporterConfig {
+            default_keyword: 1,
+            keywords_map: HashMap::new(),
+        };
+        let exporter = UserEventsExporter::new(provider_name, None, exporter_config);
         let reenterant_processor = ReentrantLogProcessor::new(exporter);
         self.with_log_processor(reenterant_processor)
     }
