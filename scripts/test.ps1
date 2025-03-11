952f4d9c47829173ece2122a2f99917c7f2ba5ba
Pato Sandaña <psandana@gmail.com>
2025-03-11 16:47:40 -0300

    test on windows

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,2 +1,9 @@
+$ErrorActionPreference = "Stop"
+
 cargo test --manifest-path=opentelemetry-etw-logs/Cargo.toml --all-features
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
 cargo test --manifest-path=opentelemetry-etw-metrics/Cargo.toml --all-features
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+exit $LASTEXITCODE
