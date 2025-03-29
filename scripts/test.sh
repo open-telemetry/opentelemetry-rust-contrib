400577dfc585e27698d9cf4fdefcce0963d2ac83
gruebel <anton.gruebel@gmail.com>
2025-03-29 22:13:47 +0100

    simplify test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,16 +2,8 @@
 
 set -eu
 
-cargo test --all --all-features "$@" -- --test-threads=1
+echo "Running tests for all packages in workspace with --all-features"
+cargo test --workspace --all-features --tests
 
-
-cargo test --manifest-path=opentelemetry-aws/Cargo.toml --all-features
-cargo test --manifest-path=opentelemetry-contrib/Cargo.toml --all-features
-
-cargo test --manifest-path=opentelemetry-datadog/Cargo.toml --all-features
-cargo test --manifest-path=opentelemetry-stackdriver/Cargo.toml --all-features
-
-cargo test --manifest-path=opentelemetry-user-events-logs/Cargo.toml --all-features
-cargo test --manifest-path=opentelemetry-user-events-metrics/Cargo.toml --all-features
-
-cargo test --manifest-path=opentelemetry-resource-detectors/Cargo.toml --all-features
+echo "Running doctests for all packages in workspace with --all-features"
+cargo test --workspace --all-features --doc
