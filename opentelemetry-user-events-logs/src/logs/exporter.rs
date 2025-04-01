1363ec206ac0090451603218c164dabd159fdc9c
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2025-04-01 10:37:32 -0700

    fix

--- opentelemetry-user-events-logs/src/logs/exporter.rs
+++ opentelemetry-user-events-logs/src/logs/exporter.rs
@@ -190,7 +190,8 @@ impl UserEventsExporter {
                 // for each event.
                 // TODO: What if the event name is not provided? "Log" is used as default.
                 // TODO: Should event_tag be non-zero?
-                eb.reset(log_record.event_name().unwrap_or("Log"), 0);
+                let event_name = log_record.event_name().filter(|s| !s.is_empty()).unwrap_or("Log");
+                eb.reset(event_name, 0);
 
                 eb.add_value("__csver__", 1024, FieldFormat::UnsignedInt, 0); // 0x400 in hex
 
