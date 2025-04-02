d709269f0ec3b9bf708bcddc60016755abd4d4b2
Pato Sandaña <psandana@gmail.com>
2025-04-03 01:51:13 +0200

    time as string

--- opentelemetry-etw-logs/src/logs/exporter/part_a.rs
+++ opentelemetry-etw-logs/src/logs/exporter/part_a.rs
@@ -1,6 +1,5 @@
 use opentelemetry_sdk::logs::TraceContext;
 use std::time::SystemTime;
-use tracelogging::win_filetime_from_systemtime;
 use tracelogging_dynamic as tld;
 
 pub fn populate_part_a(
@@ -86,12 +85,19 @@ fn populate_time(
     log_record: &opentelemetry_sdk::logs::SdkLogRecord,
     field_tag: u32,
 ) {
+    use chrono::{DateTime, Utc};
+
     let event_time: SystemTime = log_record
         .timestamp()
         .or(log_record.observed_timestamp())
         .unwrap_or_else(SystemTime::now);
 
-    let timestamp = win_filetime_from_systemtime!(event_time);
+    let timestamp: DateTime<Utc> = event_time.into();
     // TODO: time should be string
-    event.add_filetime("time", timestamp, tld::OutType::Default, field_tag);
+    event.add_str8(
+        "time",
+        timestamp.to_rfc3339().as_str(),
+        tld::OutType::Default,
+        field_tag,
+    );
 }
