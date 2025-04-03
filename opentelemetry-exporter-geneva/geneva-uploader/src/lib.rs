f91a04e9975696976045e7b550030e51a428a9e3
Lalit Kumar Bhasin <labhas@microsoft.com>
2025-04-03 06:40:41 +0000

    add unit-test

--- opentelemetry-exporter-geneva/geneva-uploader/src/lib.rs
+++ opentelemetry-exporter-geneva/geneva-uploader/src/lib.rs
@@ -1,6 +1,16 @@
 mod config_service;
+mod ingestion_service;
 mod uploader;
 /*pub use config_service::{
     AuthMethod, GenevaConfigClient, GenevaConfigClientConfig, GenevaConfigClientError, IngestionGatewayInfo,
 };*/
-pub use uploader::{create_uploader, GenevaUploader};
+pub use config_service::client::{
+    AuthMethod, GenevaConfigClient, GenevaConfigClientConfig, GenevaConfigClientError,
+    IngestionGatewayInfo,
+};
+
+pub use ingestion_service::uploader::{
+    GenevaUploader, GenevaUploaderConfig, GenevaUploaderError, IngestionResponse, Result,
+};
+
+pub use uploader::{create_uploader, GenevaUploader as Uploader};
