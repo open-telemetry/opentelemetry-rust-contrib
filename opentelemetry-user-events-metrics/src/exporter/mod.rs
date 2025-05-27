0f7d965afce4c5b2d92a3e61cb7a9149bc5ef8ef
Cijo Thomas <cijo.thomas@gmail.com>
2025-05-27 15:27:35 -0700

    remove unnecessary pub

--- opentelemetry-user-events-metrics/src/exporter/mod.rs
+++ opentelemetry-user-events-metrics/src/exporter/mod.rs
@@ -89,7 +89,7 @@ impl Debug for MetricsExporter {
     }
 }
 
-pub(crate) fn to_nanos(time: SystemTime) -> u64 {
+fn to_nanos(time: SystemTime) -> u64 {
     time.duration_since(UNIX_EPOCH)
         .unwrap_or_else(|_| Duration::from_secs(0))
         .as_nanos() as u64
