67154faf8203f73072989b526123830f5ad78045
Cijo Thomas <cijo.thomas@gmail.com>
2025-07-08 19:16:08 -0700

    static

--- opentelemetry-user-events-logs/src/logs/exporter.rs
+++ opentelemetry-user-events-logs/src/logs/exporter.rs
@@ -159,7 +159,7 @@ impl UserEventsExporter {
     }
 
     /// Gets the event name from the log record using the provided callback or returns "Log" if no callback is set
-    fn get_event_name<'a>(&self, record: &'a opentelemetry_sdk::logs::SdkLogRecord) -> &'a str {
+    fn get_event_name(&self, record: &opentelemetry_sdk::logs::SdkLogRecord) -> &'static str {
         match &self.event_name_callback {
             // TODO: Add validation that the name is valid.
             Some(callback) => (callback)(record),
