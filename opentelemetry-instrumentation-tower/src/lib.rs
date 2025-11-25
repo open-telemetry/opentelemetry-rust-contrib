1a0a77ee07cc3dc68301b224ebed4559ca516c3e
Jan Steinke <me@janxyz.de>
2025-11-25 23:01:48 +0100

    add ContextGuard import

--- opentelemetry-instrumentation-tower/src/lib.rs
+++ opentelemetry-instrumentation-tower/src/lib.rs
@@ -15,6 +15,7 @@ use opentelemetry::global::{self, BoxedTracer};
 use opentelemetry::metrics::{Histogram, MeterProvider, UpDownCounter};
 use opentelemetry::trace::{SpanKind, Status, TraceContextExt, Tracer, TracerProvider};
 use opentelemetry::Context as OtelContext;
+use opentelemetry::ContextGuard;
 use opentelemetry::KeyValue;
 use opentelemetry_http::HeaderExtractor;
 use opentelemetry_semantic_conventions as semconv;
